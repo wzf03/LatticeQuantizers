3049a079d4d1cb0bf6b49f3db71e466946b629f9
Zhuofeng Wang <wangzf2003@stu.pku.edu.cn>
2024-12-28 00:10:04 +0800

    Improve cli.

--- sgd.py
+++ sgd.py
@@ -1,10 +1,9 @@
 import argparse
 import datetime
+import logging
 from pathlib import Path
 
 import numpy as np
-
-from lattice_quantizer.optimizer import SGDLatticeQuantizerOptimizer
 
 
 def parse_arg() -> argparse.Namespace:
@@ -21,11 +20,18 @@ def parse_arg() -> argparse.Namespace:
     )
 
     parser.add_argument(
+        "-m",
+        "--mode",
+        type=str,
+        default="medium",
+        help="Mode of the optimization",
+    )
+
+    parser.add_argument(
         "-mu0",
         "--initial_step_size",
         type=float,
         required=False,
-        default=0.001,  # Medium:0.001; Fast: 0.005; Slow: 0.0005
         help="Initial step size for the stochastic gradient descent",
     )
 
@@ -34,7 +40,6 @@ def parse_arg() -> argparse.Namespace:
         "--radio",
         type=int,
         required=False,
-        default=500,  # Medium: 500; Fast: 200; Slow: 1000
         help="Ratio between initial and final step size",
     )
 
@@ -43,7 +48,6 @@ def parse_arg() -> argparse.Namespace:
         "--steps",
         type=int,
         required=False,
-        default=10_000_000,  # Medium: 10000000; Fast: 1000000; Slow: 100000000
         help="Number of steps for the stochastic gradient descent",
     )
 
@@ -52,7 +56,6 @@ def parse_arg() -> argparse.Namespace:
         "--reduction_interval",
         type=int,
         required=False,
-        default=100,
         help="Interval between consecutive reductions",
     )
 
@@ -70,19 +73,62 @@ def main():
     args = parse_arg()
     output_dir = Path(args.output_dir)
     output_dir.mkdir(parents=True, exist_ok=True)
+    output_suffix = f"{args.dimension}"
+
+    if args.mode == "medium":
+        initial_step_size = 0.001
+        radio = 500
+        steps = 10_000_000
+        reduction_interval = 100
+        output_suffix += "_medium"
+    elif args.mode == "fast":
+        initial_step_size = 0.005
+        radio = 200
+        steps = 1_000_000
+        reduction_interval = 100
+        output_suffix += "_fast"
+    elif args.mode == "slow":
+        initial_step_size = 0.0005
+        radio = 1000
+        steps = 100_000_000
+        reduction_interval = 100
+        output_suffix += "_slow"
+    else:
+        logging.fatal(
+            f"Mode {args.mode} not recognized, accepted values are 'medium', 'fast' and 'slow'"
+        )
+        return
+
+    if args.initial_step_size is not None:
+        initial_step_size = args.initial_step_size
+        output_suffix += f"_mu0[{args.initial_step_size}]"
+
+    if args.radio is not None:
+        radio = args.radio
+        output_suffix += f"_v[{args.radio}]"
+
+    if args.steps is not None:
+        steps = args.steps
+        output_suffix += f"_T[{args.steps}]"
+
+    if args.reduction_interval is not None:
+        reduction_interval = args.reduction_interval
+        output_suffix += f"_Tr[{args.reduction_interval}]"
+
+    from lattice_quantizer.optimizer import SGDLatticeQuantizerOptimizer
 
     optimzer = SGDLatticeQuantizerOptimizer(
         args.dimension,
-        args.initial_step_size,
-        args.radio,
-        args.steps,
-        args.reduction_interval,
+        initial_step_size,
+        radio,
+        steps,
+        reduction_interval,
     )
 
-    B = optimzer.optimize()
+    result = optimzer.optimize()
     np.savetxt(
-        output_dir / f"quantizer_{args.dimension}_{datetime.datetime.now()}.txt",
-        B,
+        output_dir / f"quantizer_{output_suffix}_{datetime.datetime.now()}.txt",
+        result,
     )
 
 
