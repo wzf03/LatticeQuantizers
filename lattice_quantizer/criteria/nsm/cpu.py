7c833795cf4e41194c2047d507d767de91398e98
Zhuofeng Wang <wangzf2003@stu.pku.edu.cn>
2024-12-28 14:12:13 +0800

    Implement serial version of nsm.

--- lattice_quantizer/criteria/nsm/cpu.py
+++ lattice_quantizer/criteria/nsm/cpu.py
@@ -19,7 +19,7 @@ def _sample(
     parallel=True,
     cache=True,
 )
-def nsm_cpu(
+def _nsm_cpu_batched(
     basis: np.ndarray,
     num_samples: int,
     batch_size: int,
@@ -39,3 +39,38 @@ def nsm_cpu(
     nsm = np.mean(g)
     var = (np.mean(g**2) - nsm**2) / (num_samples - 1)
     return nsm, var
+
+
+@njit(
+    [types.Tuple((float64, float64))(float64[:, :], int32, types.npy_rng)],
+    cache=True,
+)
+def _nsm_cpu(
+    basis: np.ndarray,
+    num_samples: int,
+    rng: np.random.Generator,
+) -> float:
+    n = basis.shape[0]
+    g = np.zeros(num_samples)
+    v = np.prod(np.diag(basis))
+    basis = v ** (-1 / n) * basis
+
+    for i in prange(num_samples):
+        z = rng.random(n)
+        g[i] = _sample(basis, z) / n
+
+    nsm = np.mean(g)
+    var = (np.mean(g**2) - nsm**2) / (num_samples - 1)
+    return nsm, var
+
+
+@njit(cache=True)
+def nsm_cpu(
+    basis: np.ndarray,
+    num_samples: int,
+    rng: np.random.Generator,
+    batch_size: int = 0,
+) -> float:
+    if batch_size == 0:
+        return _nsm_cpu(basis, num_samples, rng)
+    return _nsm_cpu_batched(basis, num_samples, batch_size, rng)
--- tests/criteria/test_nsm.py
+++ tests/criteria/test_nsm.py
@@ -1,35 +1,52 @@
 import numpy as np
+import pytest
 
 from lattice_quantizer.criteria import baseline, nsm
 
 
-def test_nsm_cpu_cubic():
+@pytest.mark.parametrize("batch_size", [0, 1024])
+def test_nsm_cpu_cubic(batch_size: int):
     basis = np.eye(2, dtype=float)
-    result, _ = nsm.nsm_cpu(basis, 100000, 1024, np.random.default_rng(0))
+    result, _ = nsm.nsm_cpu(
+        basis, 100000, np.random.default_rng(0), batch_size=batch_size
+    )
     assert np.allclose(result, 1 / 12, atol=1e-2)
 
     basis = np.eye(3, dtype=float)
-    result, _ = nsm.nsm_cpu(basis, 100000, 1024, np.random.default_rng(0))
+    result, _ = nsm.nsm_cpu(
+        basis, 100000, np.random.default_rng(0), batch_size=batch_size
+    )
     assert np.allclose(result, 1 / 12, atol=1e-2)
 
     basis = np.eye(4, dtype=float)
-    result, _ = nsm.nsm_cpu(basis, 100000, 1024, np.random.default_rng(0))
+    result, _ = nsm.nsm_cpu(
+        basis, 100000, np.random.default_rng(0), batch_size=batch_size
+    )
     assert np.allclose(result, 1 / 12, atol=1e-2)
 
 
-def test_nsm_cpu_known():
+@pytest.mark.parametrize("batch_size", [0, 1024])
+def test_nsm_cpu_known(batch_size: int):
     basis = baseline.B_Z
-    result, _ = nsm.nsm_cpu(basis, 100000, 1024, np.random.default_rng(0))
+    result, _ = nsm.nsm_cpu(
+        basis, 100000, np.random.default_rng(0), batch_size=batch_size
+    )
     assert np.allclose(result, 0.0833, atol=1e-2)
 
     basis = baseline.B_A2
-    result, _ = nsm.nsm_cpu(basis, 100000, 1024, np.random.default_rng(0))
+    result, _ = nsm.nsm_cpu(
+        basis, 100000, np.random.default_rng(0), batch_size=batch_size
+    )
     assert np.allclose(result, 0.0801, atol=1e-2)
 
     basis = baseline.B_D3
-    result, _ = nsm.nsm_cpu(basis, 100000, 1024, np.random.default_rng(0))
+    result, _ = nsm.nsm_cpu(
+        basis, 100000, np.random.default_rng(0), batch_size=batch_size
+    )
     assert np.allclose(result, 0.0789, atol=1e-2)
 
     basis = baseline.B_D4
-    result, _ = nsm.nsm_cpu(basis, 100000, 1024, np.random.default_rng(0))
+    result, _ = nsm.nsm_cpu(
+        basis, 100000, np.random.default_rng(0), batch_size=batch_size
+    )
     assert np.allclose(result, 0.0766, atol=1e-2)
