13ca4d17064b740c2b78339128151f83b70d901c
jiacongfang <jiacongfang0411@gmail.com>
2025-01-10 18:13:34 +0800

    Update: config

--- draw_figs.py
+++ draw_figs.py
@@ -14,27 +14,43 @@ def tensorboard_smoothing(x, smooth=0.6):
 
 
 fig, ax1 = plt.subplots(1, 1)  # a figure with a 2x1 grid of Axes
-len_mean_single = pd.read_csv("./logs/quantizer_12_medium_v[1000]_T[1000000]_b[1].csv")
-len_mean_batch = pd.read_csv("./logs/quantizer_12_medium_v[1000]_T[1000000]_b[8].csv")
+default_single = pd.read_csv(
+    "./data/compare_batch[1]_batch[8]/logs/quantizer_12_medium_v[1000]_T[1000000]_b[1].csv"
+)
+default_batch = pd.read_csv(
+    "./data/compare_batch[1]_batch[8]/logs/quantizer_12_medium_v[1000]_T[1000000]_b[8].csv"
+)
+cosine_batch = pd.read_csv(
+    "./data/compare_batch[1]_batch[8]/logs/quantizer_n12_T1000000_B8.csv"
+)
+
 
 ax1.spines["top"].set_visible(False)
 ax1.spines["right"].set_visible(False)
 
 ax1.plot(
-    len_mean_single["Step"],
-    tensorboard_smoothing(len_mean_single["Value"], smooth=0.75),
+    default_single["Step"],
+    tensorboard_smoothing(default_single["Value"], smooth=0.75),
     color="#3399FF",
 )
 
 ax1.plot(
-    len_mean_batch["Step"],
-    tensorboard_smoothing(len_mean_batch["Value"], smooth=0.75),
+    default_batch["Step"],
+    tensorboard_smoothing(default_batch["Value"], smooth=0.75),
     color="#FF6666",
 )
 
-ax1.legend(["batch_size = 1", "batch_size = 8"], loc="upper right")
+ax1.plot(
+    cosine_batch["Step"],
+    tensorboard_smoothing(cosine_batch["Value"], smooth=0.75),
+    color="gray",
+)
 
-# ax1.set_xticks(np.arange(0, 24, step=2))
+ax1.legend(
+    ["medium, batch_size = 1", "medium, batch_size = 8", "cosine, batch_size = 8"],
+    loc="upper right",
+)
+
 ax1.set_xlabel("timesteps")
 ax1.set_ylabel("NSM(steps)", color="black")
 
